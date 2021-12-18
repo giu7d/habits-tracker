14f2a7759478fb1d4d3436a0d3235d486e2c66aa
Giuseppe Setem <giuseppe7d@gmail.com>
2021-12-18 11:39:16 -0300

    feat: setup .env and configuration

--- config/test.exs
+++ config/test.exs
@@ -6,17 +6,17 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :habits_tracker, HabitsTracker.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "habits_tracker_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: System.get_env("DATABASE_USER") || "postgres",
+  password: System.get_env("DATABASE_PASS") || "postgres",
+  hostname: System.get_env("DATABASE_HOST") || "localhost",
+  database: System.get_env("DATABASE_NAME") || "habits_tracker_test",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :habits_tracker, HabitsTrackerWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4002],
+  http: [port: 4002],
   secret_key_base: "YcdJOz5w9pp/aHVOMoJ5P0k027dfWPUGIpYq71N6lSqMXQlT3zec7dQu7shryY5U",
   server: false
 
