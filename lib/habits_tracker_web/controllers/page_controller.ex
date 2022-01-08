296cea0c3c49cef2958912dc30e5c268c2e95fbe
Giuseppe Setem <giuseppe7d@gmail.com>
2022-01-08 14:25:39 -0300

    refactor: remove default files

--- lib/habits_tracker_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule HabitsTrackerWeb.PageController do
-  use HabitsTrackerWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- lib/habits_tracker_web/views/page_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule HabitsTrackerWeb.PageView do
-  use HabitsTrackerWeb, :view
-end
