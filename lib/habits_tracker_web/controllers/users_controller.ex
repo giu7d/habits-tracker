e6e200d57cd0ffd679b173212ecb6f135a5a158c
Giuseppe Setem <giuseppe7d@gmail.com>
2022-01-08 14:50:51 -0300

    refactor(user): remove controller io inspect

--- lib/habits_tracker_web/controllers/users_controller.ex
+++ lib/habits_tracker_web/controllers/users_controller.ex
@@ -5,9 +5,6 @@ defmodule HabitsTrackerWeb.UsersController do
 
   def create(conn, params) do
     with {:ok, %User{} = user} <- HabitsTracker.create_user(params) do
-      IO.inspect("HabitsTrackerWeb.UsersController")
-      IO.inspect(user)
-
       conn
       |> put_status(:created)
       |> render("create_user.json", %{user: user})
